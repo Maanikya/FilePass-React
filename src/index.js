d988b6398e7726b3b7bd4a7202ea46b25cc2a9d0
Maanikya <maanikya.acharya@gmail.com>
2023-07-24 23:16:35 +0530

    Connected Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_MEASUREMENT_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+const SendAnalytics = () => {
+    ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname,
+    });
+}
+
+reportWebVitals(SendAnalytics);
